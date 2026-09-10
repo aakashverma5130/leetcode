7ce031c037fdc65f72240a7df850908c12cb96c8
aakashverma5130 <aakashverma5130@gmail.com>
2026-09-10 22:28:50 +0530

    Time: 4 ms (52.92%), Space: 15 MB (28.57%) - LeetHub

--- LeetCode/Easy/0001-two-sum/0001-two-sum.cpp
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int low = 0,sum = 0;
-        int high = nums.size()-1;
-        vector<pair<int,int>>nums2;
-        for(int i = 0; i < nums.size(); i++) {
-        nums2.push_back({nums[i], i});
-    }
-        sort(nums2.begin(),nums2.end());
-        while(low < high){
-            sum = nums2[low].first + nums2[high].first;
-            if(sum == target){
-                return{nums2[low].second,nums2[high].second};
+        map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+            int num = target - nums[i];
+            if(mp.find(num) != mp.end()){
+                return {mp[num],i};
             }
-            else if(sum < target){
-                  low++;
-            }
-            else{
-                 high--;
-            }
+            mp[nums[i]] = i;
         }
         return{};
     }
