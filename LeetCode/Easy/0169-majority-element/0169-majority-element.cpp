7f9cb2c89e1cd58da64c8596ad973d3629aac0d7
aakashverma5130 <aakashverma5130@gmail.com>
2026-09-11 14:43:10 +0530

    Time: 0 ms (100%), Space: 28.5 MB (14.74%) - LeetHub

---
+++ LeetCode/Easy/0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int>hash;
+        for(int i=0;i<n;i++){
+            hash[nums[i]]++;
+        }
+        for(int i=0;i<n;i++){
+            if(hash[nums[i]] > n/2){
+                return nums[i];
+            }
+        }
+        return 0;
+    }
+};
