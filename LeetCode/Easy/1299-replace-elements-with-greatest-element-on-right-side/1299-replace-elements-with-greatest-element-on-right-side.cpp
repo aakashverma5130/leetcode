5322c05a7fc0c50f30038ba56a21e4716bd1113b
aakashverma5130 <aakashverma5130@gmail.com>
2026-09-18 19:39:03 +0530

    Time: 1 ms (52.5%), Space: 72.5 MB (95.57%) - LeetHub

--- LeetCode/Easy/1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
+++ LeetCode/Easy/1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.cpp
@@ -6,9 +6,8 @@ public:
         vector<int>ans(n);
         for(int i=n-1;i>=0;i--){
             ans[i] = maxi;
-            if(arr[i] > maxi){
-                maxi = max(maxi,arr[i]);
-            }
+            maxi = max(maxi,arr[i]);
+            
         }
         //ans[n-1] = -1;
         return ans;
